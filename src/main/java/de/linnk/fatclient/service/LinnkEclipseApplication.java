744519a9d355346aab7b9387e85d5fd3338a1d83
mxro <mxro@nowhere.com>
2010-12-03 19:42:43 +0000

    Prepare release for Linnk using an eclipse.exe launcher.
    There were a number of problems regarding unused dependencies, had to remove the ones using ;resolution:=optional as the eclipse launcher did not seem to understand them

---
+++ src/main/java/de/linnk/fatclient/service/LinnkEclipseApplication.java
@@ -0,0 +1,18 @@
+package de.linnk.fatclient.service;
+
+import org.eclipse.equinox.app.IApplicationContext;
+
+public class LinnkEclipseApplication implements org.eclipse.equinox.app.IApplication {
+
+	@Override
+	public Object start(IApplicationContext context) throws Exception {
+		
+		return null;
+	}
+
+	@Override
+	public void stop() {
+		
+	}
+
+}
